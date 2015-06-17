91291666f90c4e8a18d0260af40c7013a492e1ea
aub59 <irhlam.ayoub@gmail.com>
2015-06-18 01:36:48 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://shop-aub59.rhcloud.com';
+/* $config['base_url']	= 'http://shop-aub59.rhcloud.com/'; /*
 
 /*
 |--------------------------------------------------------------------------
